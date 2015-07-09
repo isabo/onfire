c03bf1f85d7e0748b6407ced61ada25c55c5ba9d
isabo <isabo@claritude.com>
2015-07-09 15:51:20 +0300

    Add testing tools

---
+++ test/mockfirebase.js
@@ -0,0 +1,10 @@
+// Replace Firebase with a mock, even in require()ed modules.
+var proxyquire = require('proxyquire');
+var MockFirebase = require('mockfirebase').MockFirebase;
+var onfire = proxyquire('../dist/onfire.min', {
+    firebase: function (url) {
+        return (new MockFirebase(url));
+    }
+});
+
+module.exports.MockFirebase = MockFirebase;

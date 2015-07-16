c0db8f93d2fa17f687cbb2d718b205262feee563
isabo <isabo@claritude.com>
2015-07-16 16:23:37 +0300

    Test Model getters/setters

--- test/model_test.js
+++ test/model_test.js
@@ -41,17 +41,64 @@ test('Simple model', function (t) {
         thing.whenLoaded().
             then(function() {
 
+                // Test basics after loading.
                 t.equal(thing.exists(), true, '.exists() is true after loading');
                 t.equal(thing.hasChanges(), false, '.hasChanges() is still false after loading');
 
+                // Test the getters.
                 for (var p in testData) {
-                    t.equal(thing[p](), testData[p], p + ' === ' + testData[p]);
+                    t.equal(thing[p](), testData[p], '.' + p + '() === ' + testData[p]);
+                    t.equal(thing.get(p), testData[p], '.get(' + p + ') === ' + testData[p]);
                 }
+
+                // Test the setters.
+                for (p in schema) {
+                    var current = thing[p]();
+                    var toAssign;
+                    switch (schema[p]) {
+                        case 'number':
+                            toAssign = current+1;
+                            break;
+                        case 'string':
+                            toAssign = current+'x';
+                            break;
+                        case 'boolean':
+                            toAssign = !current;
+                    }
+                    thing[p](toAssign);
+                }
+
+                t.ok(thing.hasChanges(), 'Model has outstanding changes');
+
+                thing.save().then(function() {
+                    // Compare the model's values with the expected values.
+                    for (var p in schema) {
+                        switch (schema[p]) {
+                            case 'number':
+                                expected = testData[p]+1;
+                                break;
+                            case 'string':
+                                expected = testData[p]+'x';
+                                break;
+                            case 'boolean':
+                                expected = !testData[p];
+                        }
+                        t.equal(thing[p](), expected, '.' + p + '(value) updates model correctly');
+                        t.equal(ref.child(p).getData(), expected, '.' + p + '(value) updates database correctly');
+                    }
+
+                }, function(err) {
+                    t.error(err, 'Failed to save the changes');
+                });
+
+
+
             }, function(err) {
                 t.error(err);
+            }).
+            then(function() {
+                t.end();
             });
-
-        t.end();
     });
     ref.flush();
 });
@@ -60,4 +107,5 @@ test('Simple model', function (t) {
 // .get('non-existent property') --> throws exception.
 // .getBasicValue('non-existent property') --> throws exception.
 // .getModel('non-existent property or not a model') --> throws exception.
-// Missing boolean = false;
+// Missing boolean = false; Missing number = 0? --> only with a definition of default in schema? string=''?
+// overloaded getter name: warning when generating constructor, still works with get/set.

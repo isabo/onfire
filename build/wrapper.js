2d5cf18a95ead1ba618a4e3e7fc26ab82f60f3f4
isabo <isabo@claritude.com>
2015-12-07 08:12:53 +0200

    Add licence to compiled code and externs file

---
+++ build/wrapper.js
@@ -0,0 +1,6 @@
+/**
+ * OnFire
+ * Copyright (c) 2013 - 2015 Itzy Sabo
+ * Licensed under the MIT License: https://github.com/isabo/onfire/blob/master/LICENSE
+ */
+(function(){%output%})();//# sourceMappingURL=./onfire.min.js.map

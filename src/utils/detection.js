df36577dd72440c7e581d304cf86d6edd036bed3
isabo <isabo@claritude.com>
2015-12-24 10:30:12 +0200

    Add additional test for Node environment
    module !== this.module in Node, but this can be true in a browser

--- src/utils/detection.js
+++ src/utils/detection.js
@@ -9,4 +9,5 @@ goog.provide('onfire.utils.detection');
 /**
  * @type {boolean}
  */
-onfire.utils.detection.IS_NODEJS = (typeof module !== 'undefined' && !!module.exports);
+onfire.utils.detection.IS_NODEJS =
+    (typeof module !== 'undefined' && module !== this.module && !!module.exports);
